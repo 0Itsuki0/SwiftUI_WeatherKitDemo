b33d37bd19ba0d1c2736c5659d6b0487aed938d8
itsuki <itsuki.enjyoy@gmail.com>
2025-08-04 08:27:53 +0900

    Initial Commit

---
+++ WeatherKitDemo/WeatherKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherKitDemoApp.swift
+//  WeatherKitDemo
+//
+//  Created by Itsuki on 2025/08/04.
+//
+
+import SwiftUI
+
+@main
+struct WeatherKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
